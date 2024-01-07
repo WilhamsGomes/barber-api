d9a726a864362399efe18180f845312f3d4bdebd
Wilhams Gomes <71606861+WilhamsGomes@users.noreply.github.com>
2024-01-07 17:03:38 -0300

    feat: module services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { BarbersModule } from './modules/barbers/barbers.module';
 import { DatabaseModule } from './shared/database/database.module';
+import { ServicesModule } from './modules/services/services.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, BarbersModule],
+  imports: [UsersModule, DatabaseModule, BarbersModule, ServicesModule],
   controllers: [],
   providers: [],
 })
