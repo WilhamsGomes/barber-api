01d758506a0acf6b1670e45866cf61d1b50eccb9
Wilhams Gomes <71606861+WilhamsGomes@users.noreply.github.com>
2024-01-15 21:27:48 -0300

    feat: service to barber

--- src/modules/services/services.controller.ts
+++ src/modules/services/services.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ServicesService } from './services.service';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
@@ -7,27 +15,27 @@ import { UpdateServiceDto } from './dto/update-service.dto';
 export class ServicesController {
   constructor(private readonly servicesService: ServicesService) {}
 
-  @Post()
+  @Post(':barberId')
   create(@Body() createServiceDto: CreateServiceDto) {
     return this.servicesService.create(createServiceDto);
   }
 
-  @Get()
+  @Get(':barberId')
   findAll() {
     return this.servicesService.findAll();
   }
 
-  @Get(':id')
+  @Get(':barberId/:id')
   findOne(@Param('id') id: string) {
     return this.servicesService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch(':barberId/:id')
   update(@Param('id') id: string, @Body() updateServiceDto: UpdateServiceDto) {
     return this.servicesService.update(+id, updateServiceDto);
   }
 
-  @Delete(':id')
+  @Delete(':barberId/:id')
   remove(@Param('id') id: string) {
     return this.servicesService.remove(+id);
   }
