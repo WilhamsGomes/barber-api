f490dc19f73a10b6cc326bb466327e20a6c6461a
Wilhams Gomes <71606861+WilhamsGomes@users.noreply.github.com>
2024-03-17 21:59:53 -0300

    feat: module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from './modules/users/users.module';
 import { BarbersModule } from './modules/barbers/barbers.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { ServicesModule } from './modules/services/services.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, BarbersModule, ServicesModule],
+  imports: [UsersModule, DatabaseModule, BarbersModule, ServicesModule, AuthModule],
   controllers: [],
   providers: [],
 })
